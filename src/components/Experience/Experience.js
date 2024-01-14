78eaf8cdbcdda62f1855d58d53c743d2a8936058
Andrii Rudiuk <dron74961@gmail.com>
2024-01-14 16:59:36 +0200

    Updated EXPERIENCE

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -15,52 +15,61 @@ export const Experience = () => {
 
         <WorkWrapper>
           <Work>
-            <Title>Company Name | Front-End developer</Title>
+            <Title>My-Cowork | JavaScript developer</Title>
 
             <Description>
-              <Label>- Develop and implement shaders, algorithms and optimize object models for the application lifecycle</Label><br />
-              <Label>- Collaborate with the team to develop components and logic for an application for a beauty salon</Label><br />
-              <Label>- Create adaptive layouts and React components using Typescript/React with styled components</Label><br />
-              <Label>- Style layouts using CSS/Sass and add functionality with CRUD API and data storage in Redux</Label>
+              <Label>- Support company's projects</Label><br />
+              <Label>- Implement new features, fix bugs etc</Label><br />
+              <Label>- Creating social network (React, Redux, TypeScript)</Label><br />
+              <Label>- Creating Web UI Elements- Developing an app (React Native)</Label><br />
+              <Label>- Creating react components</Label><br />
+              <Label>- Styling layouts using css/sass</Label><br />
+              <Label>- Adding functionality by attaching crud api, storing data in redux</Label>
             </Description>
           </Work>
         </WorkWrapper>
 
         <WorkWrapper $justifyContent='flex-end'>
           <Work>
-            <Title>Company Name | Front-End developer</Title>
+            <Title>Dual Education Ukraine | JavaScript developer</Title>
 
             <Description>
-              <Label>- Develop and implement shaders, algorithms and optimize object models for the application lifecycle</Label><br />
-              <Label>- Collaborate with the team to develop components and logic for an application for a beauty salon</Label><br />
-              <Label>- Create adaptive layouts and React components using Typescript/React with styled components</Label><br />
-              <Label>- Style layouts using CSS/Sass and add functionality with CRUD API and data storage in Redux</Label>
+              <Label>- Development of web applications using React (Mobx), React Native technologies</Label><br />
+              <Label>- Development of mobile applications for Android and IOS using real technology</Label><br />
+              <Label>- Working with Wordpress and React</Label><br />
+              <Label>- Creation of cross-browser, semantic and correct code</Label><br />
+              <Label>- Working with a database (SQL, MySQL)</Label><br />
+              <Label>- Integration and editing of scripts on sites</Label>
             </Description>
           </Work>
         </WorkWrapper>
 
         <WorkWrapper>
           <Work>
-            <Title>Company Name | Front-End developer</Title>
+            <Title>Decision Mapper | Front-End developer</Title>
 
             <Description>
-              <Label>- Develop and implement shaders, algorithms and optimize object models for the application lifecycle</Label><br />
-              <Label>- Collaborate with the team to develop components and logic for an application for a beauty salon</Label><br />
-              <Label>- Create adaptive layouts and React components using Typescript/React with styled components</Label><br />
-              <Label>- Style layouts using CSS/Sass and add functionality with CRUD API and data storage in Redux</Label>
+              <Label>- Work to support the international trading platform</Label><br />
+              <Label>- Participation in the implementation of various modules. Most of them were developed using React with Redux</Label><br />
+              <Label>- Participation in the development of methods for analyzing the structure of entire social objects</Label><br />
+              <Label>- Developing a secure web application for preparing, sending, and receiving client relationships</Label><br />
+              <Label>- Created informational trade blocks of panels with the current price, size, total amount</Label><br />
+              <Label>- Information desk of previous transactions and others</Label>
             </Description>
           </Work>
         </WorkWrapper>
 
         <WorkWrapper $justifyContent='flex-end'>
           <Work>
-            <Title>Company Name | Front-End developer</Title>
+            <Title>Groxxy | Front-End developer</Title>
 
             <Description>
-              <Label>- Develop and implement shaders, algorithms and optimize object models for the application lifecycle</Label><br />
-              <Label>- Collaborate with the team to develop components and logic for an application for a beauty salon</Label><br />
-              <Label>- Create adaptive layouts and React components using Typescript/React with styled components</Label><br />
-              <Label>- Style layouts using CSS/Sass and add functionality with CRUD API and data storage in Redux</Label>
+              <Label>- Writing shaders, algorithms translation and optimization objects model, implementation and interaction with them in the application life cycle, query processing</Label><br />
+              <Label>- Working in a team, involving in developing the сomponents and logic for app beauty salon</Label><br />
+              <Label>- Worked by using Typescript/React/Redux + styled components</Label><br />
+              <Label>- Creating react components, creating adaptive layouts</Label><br />
+              <Label>- Styling layouts using css/sass</Label><br />
+              <Label>- Adding functionality by attaching crud api, storing data in redux</Label>
             </Description>
           </Work>
         </WorkWrapper>
