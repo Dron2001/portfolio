1d5c5810c4877f194654655a1b6b5515a98aef44
Andrii Rudiuk <dron74961@gmail.com>
2024-01-15 13:42:06 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path='/portfolio' element={<App />} />
       </Routes>
     </Router>
   </React.StrictMode>
