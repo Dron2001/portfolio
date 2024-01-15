d335d099211fbbb1fd74c08cfd04e95e0b16777c
Andrii Rudiuk <dron74961@gmail.com>
2024-01-15 12:39:02 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -6,15 +6,20 @@ import { App } from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-])
+const router = createBrowserRouter({
+  basename: '/portfolio',
+  routes: [
+    {
+      path: '/',
+      element: <App />,
+    },
+  ],
+})
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} basename='/portfolio'>
+      <App />
+    </RouterProvider>
   </React.StrictMode>
 )
